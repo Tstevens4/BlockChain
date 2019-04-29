92cef9351ddb4d4ca695193e053c3f251b414307
Alex Yares <alex@yares.net>
2019-04-29 16:18:28 -0600

    Initial UI Upload

---
+++ term-ui/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+          <div class="title">
+            <h1> Gamble up in this bitch </h1>
+          </div>
+        </div>
+    );
+}
+
+export default App;
