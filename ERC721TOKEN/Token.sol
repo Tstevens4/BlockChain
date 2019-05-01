41420bbb8f311beaad4f9de4a857f7a6a6a8b24f
Alex Yares <alex@yares.net>
2019-04-30 18:12:12 -0600

    Added fight function and arena reset function which is not yet complete.

--- ERC721TOKEN/Token.sol
+++ ERC721TOKEN/Token.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.2;
 
 import './node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 import './node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "./truffle_stuff/output.sol";
 
 
 contract Fighters is ERC721, Ownable {
@@ -19,7 +20,7 @@ contract Fighters is ERC721, Ownable {
   }
   //all the fighters that are created
   Fighter[] internal fighters;
-  //fighters qued for the fight (2 at a time)
+  //fighters queued for the fight (2 at a time)
   uint[] QuedFighterIds;
 
   // mapping token owners to tokens
@@ -29,10 +30,14 @@ contract Fighters is ERC721, Ownable {
   //bidder addreses
   address[] bidders;
   mapping(address => uint) public bids;
-  //mapping bidder addreses to qued fighter Ids
+  //mapping bidder addresses to qued fighter Ids
   mapping(address => uint[]) public idsBidOn;
-  //Totalbids that have been made 
+  //Total bids that have been made
   uint256 TotalAmountBet;
+
+  // Track winner and game state
+  uint winningFighterID;
+  bool fightOver = false;
 
 
 
@@ -48,14 +53,14 @@ function CreateFighter() external onlyOwner{
  // Gets you a fighter with random stats lowest as rare up to to Legendary
   function buyLegendaryFighter() public payable {
     
-    require(msg.value >= oneEther * 2 , "you need to bid atleast 2 ethetTo get a chance at Legendary Fighters" );
+    require(msg.value >= oneEther * 2 , "you need to bid at least 2 ether to get a chance at Legendary Fighters" );
     Fighter memory _fighter = Fighter(random(40) + 160, random(7) + 4, random(30) + 20, random(15) + 2 ); 
     uint _id = fighters.push(_fighter) - 1;
     _mint(msg.sender, _id);
   }
 // Gets you a fighter with random stats more likely as a rare up to Legendary
    function buyRareFighter() public payable {
-    require(msg.value >= oneEther, "you need to bid atleast 1 ether get a chance at rare Fighters" );
+    require(msg.value >= oneEther, "you need to bid at least 1 ether to get a chance at rare Fighters" );
     Fighter memory _fighter = Fighter(random(80) + 120, random(8) + 3, random(33) + 18, random(16) ); 
     uint _id = fighters.push(_fighter) - 1;
     _mint(msg.sender, _id);
@@ -63,15 +68,11 @@ function CreateFighter() external onlyOwner{
 
 // Gets you a fighter with random stats up to Legendary
    function buyFighter() public payable {
-    require(msg.value >= oneEther / 2, "you need to bid atleast half of an ether get a random Fighter" );
+    require(msg.value >= oneEther / 2, "you need to bid at least half of an ether to get a random Fighter" );
     Fighter memory _fighter = Fighter(random(100) + 100, random(10) + 1, random(34) + 16, random(16) ); 
     uint _id = fighters.push(_fighter) - 1;
     _mint(msg.sender, _id);
   }
-
-
-
-
 
   function getFighterFromId(uint id) public view returns( uint, uint, uint, uint) {
     return (fighters[id].Health, fighters[id].Speed, fighters[id].AttackPower, fighters[id].Defense);
@@ -96,13 +97,13 @@ function CreateFighter() external onlyOwner{
     QuedFighterIds.push(fighterId);
     if(QuedFighterIds.length == 2){
       betting_start=now;
-      betting_end = betting_start + 15 minutes;
+      betting_end = betting_start + 1 minutes;
     }
     return true;
   }
-  // after the fighters are in que start betting session
+  // after the fighters are in queue start betting session
   function BettingSession(uint fighterId) public payable{
-    require(QuedFighterIds.length > 1 , "The arent Enough Fighters in the que for the Betting Session To start yet ");
+    require(QuedFighterIds.length > 1 , "The aren't Enough Fighters in the que for the Betting Session To start yet ");
     require(msg.value > 1000, "Lowest bet  accepted is 1000 wei" );
     require(betting_end > now, "The betting window has closed wait for another fight" );
     require(QuedFighterIds[0] == fighterId || QuedFighterIds[1] == fighterId, "The fighter you selected does not apper to be qued for betting");
@@ -110,8 +111,64 @@ function CreateFighter() external onlyOwner{
     
     bidders.push(msg.sender);
 
-
   }
 
+  function Fight() public {
+      //Choose who fights first
+      uint[] f1 = getFighterFromId(QuedFighterIds[0]);
+      uint[] f2 = getFighterFromId(QuedFighterIds[1]);
+
+      fightOver = false;
+
+      // Access each attribute via the array
+      // Health - 0
+      // Speed - 1
+      // Attack - 2
+      // Defense - 3
+      if (f1[1] > f2[1]){
+          // F1 fights first
+          while (!fightOver){
+              f2[0] -= (f1[2] - f2[3]);
+              if (f2[0] <= 0){
+                  //Fighter is defeated
+                  winningFighterID = QuedFighterIds[0];
+                  fightOver = true;
+              }
+              f1[0] -= (f2[2] - f1[3]);
+              if (f1[0] <= 0){
+                  winningFighterID = QuedFighterIds[1];
+                  fightOver = true;
+              }
+          }
+      } else {
+          // F2 fights first
+          while(!fightOver){
+              f1[0] -= (f2[2] - f1[3]);
+              if (f1[0] <= 0){
+                  winningFighterID = QuedFighterIds[1];
+                  fightOver = true;
+              }
+              f2[0] -= (f1[2] - f2[3]);
+              if (f2[0] <= 0){
+                  winningFighterID = QuedFighterIds[0];
+                  fightOver = true;
+              }
+          }
+      }
+
+      //Fight is over, winner is set. Burn loser
+      if (QuedFighterIds[0] == winningFighterID){
+          burnToken(QuedFighterIds[1]);
+      } else {
+          burnToken(QuedFighterIds[0]);
+      }
+
+      resetArena();
+  }
+
+  function resetArena () public {
+      QuedFighterIds.length = 0;
+
+  }
 
 }
